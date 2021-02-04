cd2b7741baa3520943e82525e02906da8b2a3aa9
Emily Lim <emily.elim04@gmail.com>
2021-02-04 20:38:49 +0000

    add pool.query to addProperty to create new property listings:

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -94,7 +94,7 @@ const getAllProperties = function(options, limit = 10) {
 
   let queryString = `SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
-  JOIN property_reviews ON properties.id = property_id 
+  LEFT JOIN property_reviews ON properties.id = property_id 
   WHERE true `;
 
   if (options.city) {
@@ -142,9 +142,13 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryString = `INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;`;
+
+  let values = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code];
+
+  console.log(queryString, values)
+
+  return pool.query(queryString, values).then(data => data.rows[0]).catch(err => console.log('query error', err.stack));
+
 }
 exports.addProperty = addProperty;
