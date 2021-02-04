aa6d1f9fbfcc5bdcdf3992fc2447dcdd38c17db5
Emily Lim <emily.elim04@gmail.com>
2021-02-04 17:00:56 +0000

    add pool queries to addUser, getUserWithID and getUserWithEmail

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -21,16 +21,11 @@ const users = require('./json/users.json');
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+
+  return pool.query(`SELECT * FROM users WHERE email = $1;`, [email])
+  .then(data => data.rows[0])
+  .catch(err => console.error('query error', err.stack));
+ 
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -40,7 +35,11 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+
+  return pool.query(`SELECT * FROM users WHERE id = $1;`, [id])
+  .then(data => data.rows[0])
+  .catch(err => console.error('query error', err.stack));
+
 }
 exports.getUserWithId = getUserWithId;
 
@@ -51,10 +50,10 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  const userName = user.name;
+  const userPassword = user.password;
+  const userEmail = user.email;
+  return pool.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`, [userName, userEmail, userPassword]).then(data => data.rows[0]).catch(err => console.error('query error', err.stack));
 }
 exports.addUser = addUser;
 
@@ -81,9 +80,9 @@ exports.getAllReservations = getAllReservations;
 const getAllProperties = function(options, limit = 10) {
   return pool.query(`
   SELECT * FROM properties
-  LIMIT $1
+  LIMIT $1;
   `, [limit])
-  .then(res => res.rows);
+  .then(data => data.rows).catch(err => console.error('query error', err.stack));
 }
 exports.getAllProperties = getAllProperties;
 
