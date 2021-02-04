d7b9840bc53bc568b884d8a8a2b90a5bdcf7f4ad
Emily Lim <emily.elim04@gmail.com>
2021-02-04 17:49:51 +0000

    add pool query to getAllReservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -65,7 +65,18 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `SELECT properties.*, reservations.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN reservations ON reservations.property_id = properties.id
+  JOIN property_reviews ON property_reviews.property_id = properties.id
+  WHERE reservations.guest_id = $1 AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`
+
+  return pool.query(queryString, [guest_id, limit])
+  .then(data => data.rows)
+  .catch(err => console.error('query error', err.stack));
 }
 exports.getAllReservations = getAllReservations;
 
